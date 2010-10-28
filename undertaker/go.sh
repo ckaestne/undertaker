953f47eb333b238f5b08c939e0148ae363d1042e
Reinhard Tartler <siretart@tauware.de>
2010-10-28 17:10:10 +0200

    improve output

--- undertaker/go.sh
+++ undertaker/go.sh
@@ -2,6 +2,7 @@
 
 echo "Running on Linux Version $(git describe)"
 find . -name "*.[hcS]" | shuf > worklist
+find . -name '*dead' -exec rm -f {} +
 echo "Analyzing `wc -l < worklist` files"
 time undertaker -t `getconf _NPROCESSORS_ONLN` -b worklist
-printf "Found %s defects\n" `find . -name '*dead'`
+printf "\n\nFound %s defects\n" `find . -name '*dead'| grep globally | wc -l`
