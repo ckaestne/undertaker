5d07aa75944fe1c0b3544f9f5173e549cebda20e
Reinhard Tartler <siretart@tauware.de>
2010-11-02 13:18:53 +0100

    check against all models, not only against x86

--- undertaker/validation/scheduler-structure.c
+++ undertaker/validation/scheduler-structure.c
@@ -45,7 +45,28 @@
 /*
  * check-name: schedule structure
  * check-output-start
+I: loaded rsf model for mn10300
+I: loaded rsf model for avr32
+I: loaded rsf model for parisc
+I: loaded rsf model for frv
+I: loaded rsf model for m32r
+I: loaded rsf model for ia64
+I: loaded rsf model for alpha
+I: loaded rsf model for mips
+I: loaded rsf model for blackfin
+I: loaded rsf model for cris
+I: loaded rsf model for sh
+I: loaded rsf model for s390
+I: loaded rsf model for m68k
+I: loaded rsf model for arm
+I: loaded rsf model for powerpc
+I: loaded rsf model for h8300
+I: loaded rsf model for xtensa
+I: loaded rsf model for sparc
+I: loaded rsf model for score
+I: loaded rsf model for microblaze
+I: loaded rsf model for tile
 I: loaded rsf model for x86
-I: found 1 rsf models
+I: found 23 rsf models
  * check-output-end
  */
