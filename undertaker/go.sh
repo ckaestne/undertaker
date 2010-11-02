19d555aa1e1260c3228afb487f138441ac109abc
Reinhard Tartler <tartler@cs.fau.de>
2010-11-02 15:46:13 +0100

    scripts for nightly runs on head
    scan-head is in bitten's crontab, runs nightly and reports to
    /proj/i4vamos/users/bitten/results

--- undertaker/go.sh
+++ undertaker/go.sh
@@ -1,8 +1,14 @@
 #!/bin/bash -e
 
-echo "Running on Linux Version $(git describe)"
+version="$(git describe)"
+if [ -z "$version" ]; then
+    echo "Please run in a Linux tree"
+else
+    echo "Running on Linux Version ${version}"
+fi
+
 find . -name "*.[hcS]" | shuf > worklist
 find . -name '*dead' -exec rm -f {} +
 echo "Analyzing `wc -l < worklist` files"
 time undertaker -t `getconf _NPROCESSORS_ONLN` -b worklist
-printf "\n\nFound %s defects\n" `find . -name '*dead'| grep globally | wc -l`
+printf "\n\nFound %s global defects\n" `find . -name '*dead'| grep globally | wc -l`
