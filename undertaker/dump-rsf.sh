ac74934c6c3c1717e0c874498a596461f19894f3
Reinhard Tartler <siretart@tauware.de>
2010-11-02 12:27:31 +0100

    do not hardcode path to dumpconf in dump-rsf.sh
    ensure that /proj/i4vamos/tools/bin is in the path

--- undertaker/dump-rsf.sh
+++ undertaker/dump-rsf.sh
@@ -1,10 +1,9 @@
-#!/bin/bash
+#!/bin/sh -e
 
-DUMPER=/proj/i4vamos/users/sincero/vamos/scripts/kconfig/dumpconf
+PATH=$PATH:/proj/i4vamos/tools/bin
 
 for i in alpha arm avr32 blackfin cris frv h8300 ia64 m32r m68k m68knommu microblaze mips mn10300 parisc powerpc s390 score sh sparc tile x86 xtensa
 do
-  echo $i; 
-  env ARCH="$i" $DUMPER arch/$i/Kconfig | grep -v '^#' > kconfig-$i.rsf
+  echo $i;
+  env ARCH="$i" dumpconf arch/$i/Kconfig | grep -v '^#' > kconfig-$i.rsf
 done
-
