3805c7c00a502cce341d304e49600668ab48dbc5
Christian Dietrich <qy03fugy@stud.informatik.uni-erlangen.de>
2010-11-16 19:37:51 +0100

    dump-rsf.sh: use rsf2models to calculate models in models/ subdir

--- undertaker/dump-rsf.sh
+++ undertaker/dump-rsf.sh
@@ -1,9 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
+MODELS=${MODELS:-models}
 PATH=$PATH:/proj/i4vamos/tools/bin
+TMP=$(mktemp)
+
+mkdir -p "$MODELS"
 
 for i in alpha arm avr32 blackfin cris frv h8300 ia64 m32r m68k m68knommu microblaze mips mn10300 parisc powerpc s390 score sh sparc tile x86 xtensa
 do
-  echo $i;
-  env ARCH="$i" dumpconf arch/$i/Kconfig | grep -v '^#' > kconfig-$i.rsf
+  echo "Calculating model for $i"
+  env ARCH="$i" dumpconf arch/$i/Kconfig | grep -v '^#' > $TMP
+  rsf2model $TMP > "$MODELS/$i"
 done
+
+rm -f $TMP
