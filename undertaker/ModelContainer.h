b5b405a9f077902fdb511d3d264faca104140ccf
Reinhard Tartler <siretart@tauware.de>
2010-11-29 23:14:33 +0100

    doxygen improvement

--- undertaker/ModelContainer.h
+++ undertaker/ModelContainer.h
@@ -7,9 +7,16 @@
 
 #include "ConfigurationModel.h"
 
+/**
+ * \brief Container that maps ConfigurationModel classes to its architectures
+ *
+ * This class is basically a singleton that derives from
+ * std::map<std::string, ConfigurationModel*>. It provides a few
+ * convenience methods for model loading and lookups.
+ */
 class ModelContainer : public std::map<std::string, ConfigurationModel*> {
 public:
-    static void loadModels(std::string);
+    static void loadModels(std::string modeldir); ///< load models from the given directory
     static void loadModels(std::string, std::string); ///< for arch-specific analysis
     static void loadWhitelist(const char *file);
     static ConfigurationModel *lookupModel(const char *arch);
