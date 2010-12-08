1e1e710db5c0355e1c57335588013d876a72439b
Christian Dietrich <qy03fugy@stud.informatik.uni-erlangen.de>
2010-12-08 13:56:17 +0100

    modify testcases to use undertaker instead of cpppc

--- undertaker/validation/cpppc-handle-defined.c
+++ undertaker/validation/cpppc-handle-defined.c
@@ -3,8 +3,9 @@
 #endif
 /*
  * check-name: Handle 'defined' keyword
- * check-command: cpppc $file
+ * check-command: undertaker -j cpppc $file
  * check-output-start
+I: CPP Precondition for cpppc-handle-defined.c
 ( B0 <-> defined ( CONFIG_A ) && ( defined CONFIG_B ) )
 && ( B1 <-> defined ( CONFIG_A ) && ( ! (B0) )  )
  * check-output-end
--- undertaker/validation/cpppc-nested-blocks.c
+++ undertaker/validation/cpppc-nested-blocks.c
@@ -6,8 +6,9 @@
 #endif
 /*
  * check-name: Basic Example, 3 nested blocks
- * check-command: cpppc $file
+ * check-command: undertaker -j cpppc $file
  * check-output-start
+I: CPP Precondition for cpppc-nested-blocks.c
 ( B0 <-> CONFIG_A )
 && ( B1 <->  ( B0 )  && CONFIG_B )
 && ( B2 <->  ( B1 )  && CONFIG_C )
