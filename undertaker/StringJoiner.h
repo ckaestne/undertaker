eadc6933129d506a309b5b5b2894c7778177feb3
Christian Dietrich <qy03fugy@stud.informatik.uni-erlangen.de>
2010-12-08 14:36:10 +0100

    add doxygen comments to StringJoiner

--- undertaker/StringJoiner.h
+++ undertaker/StringJoiner.h
@@ -6,7 +6,20 @@
 #include <string>
 #include <sstream>
 
+/**
+ * \brief Helper subclass of std::deque<std::string> for convenient
+ *  concatenating strings with a separator.
+ *
+ * The behaviour is similar to the python list().join(",") construct.
+ * 
+ */
 struct StringJoiner : public std::deque<std::string> {
+    /**
+     * \brief join all strings in the container,
+     *
+     * Join all the collected strings to one string. The separator is
+     * inserted between each element.
+     */
     std::string join(const char *j) {
         std::stringstream ss;
         if (size() == 0)
@@ -23,6 +36,12 @@ struct StringJoiner : public std::deque<std::string> {
         return ss.str();
     }
 
+    /**
+     * \brief append strings to list.
+     *
+     * Appends the given value to the list of values if it isn't the
+     * empty string. "" will be ignored.
+     */
     void push_back(const value_type &x) {
         if (x.compare("") == 0)
             return;
