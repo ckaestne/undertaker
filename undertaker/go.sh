3bf5715d64137821b053ece05f3d28f57b171e39
Reinhard Tartler <Reinhard.Tartler@informatik.uni-erlangen.de>
2010-12-13 16:55:54 +0100

    run coverage statistics in daily runs

--- undertaker/go.sh
+++ undertaker/go.sh
@@ -9,6 +9,14 @@ fi
 
 find . -name "*.[hcS]" | shuf > worklist
 find . -name '*dead' -exec rm -f {} +
+
+echo "Running coverage analysis"
+time undertaker -c -t `getconf _NPROCESSORS_ONLN` -b worklist -m models -M x86 2>/dev/null |
+        grep './' > coverage.txt
+
+echo "TOP 50 variable files (format: #possible solutions, filename)"
+awk -F, '{ printf "%s %s\n", $3, $1 }' < coverage.txt | sort -n -r | head -n 50 | tee coverage.stats
+
 echo "Analyzing `wc -l < worklist` files"
 time undertaker -t `getconf _NPROCESSORS_ONLN` -b worklist -m models -M x86
 printf "\n\nFound %s global defects\n" `find . -name '*dead'| grep globally | wc -l`
