b13ab50b221ddb1abd56763f3596074bd18e180c
Christian Dietrich <qy03fugy@stud.informatik.uni-erlangen.de>
2010-12-13 14:59:55 +0100

    add testcase for interesting items command

---
+++ undertaker/validation/interessting_items.c
@@ -0,0 +1,9 @@
+/*
+ * check-name: find interesting items for symbol
+ * check-command: undertaker -j interesting -m preconditions.model CONFIG_LEVEL_C_B
+ * check-output-start
+I: loaded rsf model for preconditions
+I: Using preconditions as primary model
+CONFIG_LEVEL_C_B !CONFIG_I_DONT_GIVE_A_BLOODY_HELL_PRECONDITION CONFIG_NOT_MISSING CONFIG_TOPLEVEL_C
+ * check-output-end
+ */
