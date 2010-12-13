2a9ab38254e7b36f1f8e22dd23f58512cd1d5626
Christian Dietrich <qy03fugy@stud.informatik.uni-erlangen.de>
2010-12-13 14:57:19 +0100

    ConfigurationModel: inherit RsfReader and define findSetOfInteresstingItems as public

--- undertaker/ConfigurationModel.h
+++ undertaker/ConfigurationModel.h
@@ -9,13 +9,11 @@
 
 #include "RsfReader.h"
 
-class ConfigurationModel : protected RsfReader {
+class ConfigurationModel : public RsfReader {
 public:
     ConfigurationModel(std::ifstream &in, std::ostream &log);
 
     int doIntersect(const std::set<std::string> myset, std::ostream &out, std::set<std::string> &missing) const;
-
-protected:
     void findSetOfInterestingItems(std::set<std::string> &working) const;
 };
 
