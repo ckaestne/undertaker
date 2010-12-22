b76fb0768a4ae5a7989d5581b74521794b213977
Reinhard Tartler <Reinhard.Tartler@informatik.uni-erlangen.de>
2010-12-22 12:44:44 +0100

    don't remove rsf files

--- undertaker/dump-rsf.sh
+++ undertaker/dump-rsf.sh
@@ -14,12 +14,10 @@ mkdir -p "$MODELS"
 for i in $ARCHS
 do
   echo "Calculating model for $i"
-  env ARCH="$i" dumpconf arch/$i/Kconfig | grep -v '^#' > $TMP
-  rsf2model $TMP > "$MODELS/$i.model"
+  env ARCH="$i" dumpconf arch/$i/Kconfig | grep -v '^#' > "$MODELS/$i.rsf"
+  rsf2model "$MODELS/$i.rsf" > "$MODELS/$i.model"
 
   # Add CONFIG_$i -> !CONFIG_*
   UPCASE_ARCH=$(echo $i | tr 'a-z' 'A-Z')
   echo $ARCHS | sed "s/ *$i */ /" | tr 'a-z' 'A-Z' | sed 's/$/"/; s/\</!CONFIG_/g; s/ / \&\& /g; s/^/'"CONFIG_$UPCASE_ARCH "'"/;' >> "$MODELS/$i.model"
 done
-
-rm -f $TMP
