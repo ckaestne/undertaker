7a849cb925c6e8aa857075f4ec7276c5c80f24e3
Christian Dietrich <qy03fugy@stud.informatik.uni-erlangen.de>
2011-01-11 03:31:34 +0100

    undertaker.el: add documentation and other elisp fluff
    also move it to contrib

--- contrib/undertaker.el
+++ contrib/undertaker.el
@@ -1,17 +1,50 @@
-(require 'thingatpt)
-(require 'generic-x) ;; generic mode
+;;; undertaker --- Provide an inferior mode for undertaker
 
+;;; Copyright (C) 2010, 2011 FIXME
+
+;; Author: Christian Dietrich <stettberger@dokucode.de>
+;; Version: 0.1
+;; Keywords: cpp, tools, c-programming
+
+;; undertaker.el is free software; you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; undertaker.el is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+;; or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+;; License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with undertaker.el.  If not, see <http://www.gnu.org/licenses/>.
+
+
+;;; Commentary:
+;; Enabling the undertaker minor mode, adds keybindings for sending
+;; positions in file and symbols directly to an inferior undertaker
+;; process.  With this possibility you can easily browse the
+;; preconditions of your preprocessor blocks.
+
+
+;;; History:
+;; Januar 2010, 0.1: Initial release
+
+(require 'cl)
+(require 'thingatpt)
+
+;;; Code:
 (defvar undertaker::process nil)
 
 (defun undertaker::process-runningp ()
-  "true if undertaker process is running"
+  "True if undertaker process is running."
   (and undertaker::process
        (eq (process-status undertaker::process) 'run)))
 
 (defun undertaker::ensure-running ()
-  "Checks if undertaker is running and start it otherwise (ask the user)"
+  "Check if undertaker is running and start it otherwise (ask the user)."
   (if (not (undertaker::process-runningp))
-      (if (y-or-n-p "No undertaker running. Start it? ")
+      (if (y-or-n-p "No undertaker running.  Start it? ")
           (call-interactively 'undertaker::shell)
         (progn
           (message "No undertaker process running -- undertaker::start")
@@ -20,7 +53,7 @@
 
 (defun undertaker::send-command (command &rest args)
   "Sends a command with given args to the undertaker process.\n
- Ask for starting if it isn't running"
+Ask for starting if it isn't running"
   (when (undertaker::ensure-running)
    (if args
        (save-excursion
@@ -29,21 +62,21 @@
                                       args))))))))
 
 (defun undertaker::current-blockpc ()
-  "Send current position in file to undertaker"
+  "Send current position in file to undertaker."
   (interactive)
   (let ((position (undertaker::get-position-at-point)))
     (if position
         (undertaker::send-command "::blockpc" position))))
 
 (defun undertaker::current-symbolpc ()
-  "Send current symbol at point to the undertaker"
+  "Send current symbol at point to the undertaker."
   (interactive)
   (let ((symbol (thing-at-point 'symbol)))
     (if symbol
         (undertaker::send-command "::symbolpc" symbol))))
 
 (defun undertaker::current-interesting-symbols ()
-  "Send current symbol at point to the undertaker (with ::interesting)"
+  "Send current symbol at point to the undertaker (with ::interesting)."
   (interactive)
   (let ((symbol (thing-at-point 'symbol)))
     (if symbol
@@ -51,18 +84,18 @@
 
 
 (defun undertaker::show-process ()
-  "Show the process buffer, if process is running"
+  "Show the process buffer, if process is running."
   (if (undertaker::process-runningp)
       (display-buffer (process-buffer undertaker::process))))
 
 (defun undertaker::process-cwd ()
-  "Get the working directory of the undertaker process"
+  "Get the working directory of the undertaker process."
   (with-current-buffer
       (process-buffer undertaker::process)
     default-directory))
 
 (defun undertaker::stop ()
-  "kills the undertaker process"
+  "Kill the undertaker process."
   (interactive)
   (if (undertaker::process-runningp)
       (progn
@@ -72,7 +105,7 @@
 
 
 (defun undertaker::get-position-at-point ()
-  "In the current buffer, get filename:linum as string"
+  "In the current buffer, get filename:linum as string."
   (when (undertaker::ensure-running)
    (let ((filename (buffer-file-name (current-buffer)))
          (undertaker-path (file-truename (undertaker::process-cwd))))
@@ -84,7 +117,8 @@
                (int-to-string (line-number-at-pos (point))))))))
 
 (defun undertaker::send-string (input)
-  "Send exactly this string to the undertaker process"
+  "Send exactly this string to the undertaker process.
+Argument INPUT"
   (when (undertaker::process-runningp)
     (undertaker::shell nil)
     (let ((dir default-directory))
@@ -96,7 +130,7 @@
 
 (defvar undertaker::toggle-buffer-old nil)
 (defun undertaker::toggle-buffer ()
-    "Toggles to undertaker output buffer or back to source buffer"
+    "Toggle to undertaker output buffer or back to source buffer."
     (interactive)
     (if (and undertaker::toggle-buffer-old
              (not (eq (process-buffer undertaker::process)
@@ -108,6 +142,7 @@
         (setq undertaker::toggle-buffer-old (current-buffer))
         (pop-to-buffer "*undertaker*"))))
 
+;;;###autoload
 (define-minor-mode undertaker-mode
   "Toggle undertaker mode.
 With no argument, this command toggles the mode.
@@ -137,11 +172,13 @@ source files direct to the undertaker process.
 (define-derived-mode undertaker-shell-mode comint-mode "UT-Sh"
   "\\{undertaker-mode-map}")
 (defun undertaker::shell (command)
+  "Start an inferior undertaker shell.
+In the inferior buffer COMMAND will be executed."
   (interactive (list nil))
   (unless
       (comint-check-proc "*undertaker*")
     (if (not command)
-        (setq command (read-string "undertaker::start> " "~/vamos/undertaker/undertaker -j blockpc -")))
+        (setq command (read-string "undertaker::start> " "undertaker -j blockpc -")))
     (setq command (split-string command))
     ;; start process
     (setq comint-prompt-read-only t)
@@ -161,3 +198,7 @@ source files direct to the undertaker process.
     ("::[a-z-]+" 0 font-lock-string-face t)
     ("^I:.*$" 0 font-lock-comment-face t))
   "Additional expressions to highlight in undertaker process buffers.")
+
+(provide 'undertaker)
+
+;;; undertaker.el ends here
