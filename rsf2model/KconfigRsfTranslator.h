6eb96376f08af3667bc65aaa51610567abc4a8eb
Christian Dietrich <qy03fugy@stud.informatik.uni-erlangen.de>
2011-01-18 11:17:51 +0100

    copyright headers and license files

--- rsf2model/KconfigRsfTranslator.h
+++ rsf2model/KconfigRsfTranslator.h
@@ -1,3 +1,24 @@
+/*
+ *   rsf2model - convert dumpconf output to undertaker model format
+ *
+ * Copyright (C) 2010-2011 Frank Blendinger <fb@intoxicatedmind.net>
+ * Copyright (C) 2010-2011 Reinhard Tartler <tartler@informatik.uni-erlangen.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
 // -*- mode: c++ -*-
 #ifndef kconfigrsftranlator_h__
 #define kconfigrsftranslator_h__
--- undertaker/CloudContainer.h
+++ undertaker/CloudContainer.h
@@ -1,3 +1,25 @@
+/*
+ *   undertaker - analyze preprocessor blocks in code
+ *
+ * Copyright (C) 2009-2011 Reinhard Tartler <tartler@informatik.uni-erlangen.de>
+ * Copyright (C) 2009-2011 Julio Sincero <Julio.Sincero@informatik.uni-erlangen.de>
+ * Copyright (C) 2010-2011 Christian Dietrich <christian.dietrich@informatik.uni-erlangen.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
 // -*- mode: c++ -*-
 #ifndef sat_container_h__
 #define sat_container_h__
