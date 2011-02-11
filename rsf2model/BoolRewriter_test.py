a816d85fa24152b39abf558f238a530fc049a36e
Christian Dietrich <qy03fugy@stud.informatik.uni-erlangen.de>
2011-02-11 17:03:42 +0100

    rsf2model: integrate old bool rewriting testcases

--- rsf2model/BoolRewriter_test.py
+++ rsf2model/BoolRewriter_test.py
@@ -33,6 +33,56 @@ CRAP CARASDD"""
         rewrite("B", "CONFIG_B", False)
         rewrite("!B", "(!CONFIG_B_MODULE && !CONFIG_B)", False)
 
+        rewrite("FOO=y", "CONFIG_FOO")
+
+        rewrite("FOO=n", "(!CONFIG_FOO_MODULE && !CONFIG_FOO)")
+        rewrite("FOO=m", "CONFIG_FOO_MODULE")
+        rewrite("FOO!=y", "!CONFIG_FOO")
+        rewrite("FOO!=n", "(CONFIG_FOO_MODULE || CONFIG_FOO)")
+        rewrite("FOO!=m", "!CONFIG_FOO_MODULE")
+
+        rewrite("FOO=BAR",
+                "((CONFIG_FOO && CONFIG_BAR) || (CONFIG_FOO_MODULE && CONFIG_BAR_MODULE) "
+                + "|| (!CONFIG_FOO && !CONFIG_BAR && !CONFIG_FOO_MODULE && !CONFIG_BAR_MODULE))")
+
+        rewrite("FOO!=BAR", "((CONFIG_FOO && !CONFIG_BAR) "
+                + "|| (CONFIG_FOO_MODULE && !CONFIG_BAR_MODULE) "
+                + "|| (!CONFIG_FOO && CONFIG_BAR) "
+                + "|| (!CONFIG_FOO_MODULE && CONFIG_BAR_MODULE))")
+
+        rewrite("FOO && BAR", "(CONFIG_FOO && CONFIG_BAR)")
+
+        rewrite("FOO && BAR=HURZ",
+                "(CONFIG_FOO && "
+                + "((CONFIG_BAR && CONFIG_HURZ) || (CONFIG_BAR_MODULE && CONFIG_HURZ_MODULE) || "
+                + "(!CONFIG_BAR && !CONFIG_HURZ && !CONFIG_BAR_MODULE && !CONFIG_HURZ_MODULE)))")
+
+        rewrite("FOO && BAR=FOO",
+                "(CONFIG_FOO && "
+                + "((CONFIG_BAR && CONFIG_FOO) || (CONFIG_BAR_MODULE && CONFIG_FOO_MODULE) || "
+                + "(!CONFIG_BAR && !CONFIG_FOO && !CONFIG_BAR_MODULE && !CONFIG_FOO_MODULE)))")
+
+        rewrite("B43 && (HW_RANDOM || HW_RANDOM=B43)",
+                "(CONFIG_B43 && "
+                + "(CONFIG_HW_RANDOM || "
+                + "((CONFIG_HW_RANDOM && CONFIG_B43) || "
+                + "(CONFIG_HW_RANDOM_MODULE && CONFIG_B43_MODULE) || "
+                + "(!CONFIG_HW_RANDOM && !CONFIG_B43 && "
+                + "!CONFIG_HW_RANDOM_MODULE && !CONFIG_B43_MODULE))))")
+
+        self.rsf = RsfReader.RsfReader(open("validation/equals-module.fm"))
+
+        rewrite("SSB_POSSIBLE && SSB && (PCI || PCI=SSB)",
+                "(CONFIG_SSB_POSSIBLE && (CONFIG_SSB_MODULE || CONFIG_SSB) && "
+                + "((CONFIG_PCI_MODULE || CONFIG_PCI) || ((CONFIG_PCI && CONFIG_SSB) || "
+                + "(CONFIG_PCI_MODULE && CONFIG_SSB_MODULE) || "
+                + "(!CONFIG_PCI && !CONFIG_SSB && !CONFIG_PCI_MODULE && !CONFIG_SSB_MODULE))))")
+
+        rewrite("THINKPAD_ACPI && SND && (SND=y || THINKPAD_ACPI=SND)",
+                "(CONFIG_THINKPAD_ACPI && CONFIG_SND && "
+                + "(CONFIG_SND || ((CONFIG_THINKPAD_ACPI && CONFIG_SND) || "
+                + "(CONFIG_THINKPAD_ACPI_MODULE && CONFIG_SND_MODULE) || "
+                + "(!CONFIG_THINKPAD_ACPI && !CONFIG_SND && !CONFIG_THINKPAD_ACPI_MODULE && !CONFIG_SND_MODULE))))")
 
 
 
