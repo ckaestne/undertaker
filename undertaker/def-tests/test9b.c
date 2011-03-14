1c4cd57752381efc51c549b066eaf79f1c2ce98b
Reinhard Tartler <Reinhard.Tartler@informatik.uni-erlangen.de>
2011-03-14 16:25:40 +0100

    additional test cases

---
+++ undertaker/def-tests/test9b.c
@@ -0,0 +1,15 @@
+#ifdef X
+B0
+#ifdef A
+B2
+#endif
+
+#if defined Y
+#define A
+B5
+#endif
+
+#ifdef A
+B8
+#endif
+#endif
---
+++ undertaker/def-tests/test9c.c
@@ -0,0 +1,15 @@
+#ifdef X
+B0
+#ifdef A
+B2
+#endif
+
+#ifdef Y
+#define A
+
+#ifdef A
+B5
+#endif
+#endif
+
+#endif
