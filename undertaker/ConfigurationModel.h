9f04dd3c709d968532ce38bce2453f45f99c52a3
Christian Dietrich <christian.dietrich@informatik.uni-erlangen.de>
2011-03-31 16:28:53 +0200

    undertaker/ConfigurationModel: doIntersect also on whole expressions given as string
    - remove stream as output parameter, since we don't have any codesatSTREAM anymore

--- undertaker/ConfigurationModel.h
+++ undertaker/ConfigurationModel.h
@@ -45,9 +45,20 @@ public:
 
     ConfigurationModel(std::string name, std::ifstream &in, std::ostream &log);
 
-    int doIntersect(const std::set<std::string> myset, std::ostream &out,
-                    std::set<std::string> &missing, const Checker *c=NULL) const;
-    void findSetOfInterestingItems(std::set<std::string> &working) const;
+    int doIntersect(const std::string exp,
+                    const ConfigurationModel::Checker *c,
+                    std::set<std::string> &missing,
+                    std::string &intersected) const;
+
+    int doIntersect(const std::set<std::string> exp,
+                    const ConfigurationModel::Checker *c,
+                    std::set<std::string> &missing,
+                    std::string &intersected) const;
+
+    static std::set<std::string> itemsOfString(const std::string &str);
+
+
+    std::set<std::string> findSetOfInterestingItems(const std::set<std::string> &working) const;
     static std::string getMissingItemsConstraints(std::set<std::string> &missing);
     std::string getName() const { return _name; }
 
