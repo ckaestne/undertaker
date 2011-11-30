ckaestne/undertaker

commit 387c5e160ef2a98dd0f926762b54a391357e552b

undertaker: new Logging framework
To unify the ad-hoc logging a new Logging framework is introduced,
which can be used as a standard c++ stream. It knows 4 predefined log
levels (debug, info, warn, error).

All nesseccary places should have been updated. In the testcases only the (now)
unnesseccary -q flag was removed.

Additionally, the semeantics of the commandline option -v is changed; it
now increases the verbosity level. In order to identify the undertaker
version, the option '-V' is provided.

Change-Id: I552583c855788e8d04c0c1d8f73fdd4eaa202c18

undertaker/undertaker.cpp
@@ -41,7 +41,7 @@
 #include "SatChecker.h"
 #include "CoverageAnalyzer.h"
 #include <errno.h>
-
+#include "Logging.h"
 #include "../version.h"
 
 
@@ -79,6 +79,9 @@ void usage(std::ostream &out, const char *error) {
     out << "`undertaker' does analysis on conditinal C code.\n\n";
     out << "Usage: undertaker [OPTIONS] <file..>\n";
     out << "\nOptions:\n";
+    out << "  -V  print version information";
+    out << "  -v  increase the log level (more verbose)";
+    out << "  -q  decrease the log level (less verbose)";
     out << "  -m  specify the model(s) (directory or file)\n";
     out << "  -M  specify the main model\n";
     out << "  -w  specify a whitelist\n";
@@ -126,7 +129,7 @@ int rm_pattern(const char *pattern) {
 void process_file_coverage_helper(const char *filename) {
     CppFile file(filename);
     if (!file.good()) {
-        std::cerr << "E: failed to open file: `" << filename << "'" << std::endl;
+        logger << error << "failed to open file: `" << filename << "'" << std::endl;
         return;
     }
 
@@ -155,18 +158,18 @@ void process_file_coverage_helper(const char *filename) {
         return;
     }
 
-    std::cout << "I: Entries in missingSet: " << missingSet.size() << std::endl;
+    logger << info << "Entries in missingSet: " << missingSet.size() << std::endl;
 
     std::string pattern(filename);
     pattern.append(".config*");
     int r = rm_pattern(pattern.c_str());
 
-    std::cout << "I: Removed " << r << " old configurations matching " << pattern
-              << std::endl;
+    logger << info << "Removed " << r << " old configurations matching " << pattern
+           << std::endl;
 
     if (0 == file.size()) {
-        std::cout << "I: "
-                  << filename << "does not contain any conditional block" << std::endl;
+        logger << info << filename
+               << "does not contain any conditional block" << std::endl;
         return;
     }
 
@@ -188,9 +191,9 @@ void process_file_coverage_helper(const char *filename) {
             || coverageOutputMode == COVERAGE_MODE_ALL) {
             outf.open(outfstream.str().c_str(), std::ios_base::trunc);
             if (!outf.good()) {
-                std::cerr << "E: failed to write config in "
-                          << outfstream.str().c_str()
-                          << std::endl;
+                logger << error <<" failed to write config in "
+                       << outfstream.str().c_str()
+                       << std::endl;
                 outf.close();
                 continue;
             }
@@ -202,7 +205,7 @@ void process_file_coverage_helper(const char *filename) {
             break;
         case COVERAGE_MODE_MODEL:
             if (!model) {
-                std::cerr << "E: no model loaded but, model output mode specified" << std::endl;
+                logger << error << "no model loaded but, model output mode specified" << std::endl;
                 exit(1);
             }
             (*it).formatModel(outf, model);
@@ -231,32 +234,32 @@ void process_file_coverage_helper(const char *filename) {
 
     float ratio = 100.0 * enabled_blocks / file.size();
 
-    std::cout << "I: "
-              << filename << ", "
-              << "Found Solutions: " << solution.size() << ", " // #found solutions
-              << "Coverage: " << enabled_blocks << "/" << file.size() << " blocks enabled "
-              << "(" << ratio <<  "%)" << std::endl;
+    logger << info
+           << filename << ", "
+           << "Found Solutions: " << solution.size() << ", " // #found solutions
+           << "Coverage: " << enabled_blocks << "/" << file.size() << " blocks enabled "
+           << "(" << ratio <<  "%)" << std::endl;
 }
 
 void process_file_coverage (const char *filename) {
     boost::thread t(process_file_coverage_helper, filename);
 
     if (!t.timed_join(boost::posix_time::seconds(300)))
-        std::cerr << "E: timeout passed while processing " << filename
-                  << std::endl;
+        logger << error << "timeout passed while processing " << filename
+               << std::endl;
 }
 
 void process_file_cpppc(const char *filename) {
     CppFile s(filename);
     if (!s.good()) {
-        std::cerr << "E: failed to open file: `" << filename << "'" << std::endl;
+        logger << error << "failed to open file: `" << filename << "'" << std::endl;
         return;
     }
-    std::cout << "I: CPP Precondition for " << filename << std::endl;
+    logger << info << "CPP Precondition for " << filename << std::endl;
     try {
         std::cout << s.topBlock()->getCodeConstraints() << std::endl;
     } catch (std::runtime_error &e) {
-        std::cerr << "E: failed: " << e.what() << std::endl;
+        logger << error << "failed: " << e.what() << std::endl;
         return;
     }
 }
@@ -266,7 +269,7 @@ void process_file_blockpc(const char *filename) {
     std::string file, position;
     size_t colon_pos = fname.find_first_of(':');
     if (colon_pos == fname.npos) {
-        std::cerr << "E: invalid format for block precondition" << std::endl;
+        logger << error << "invalid format for block precondition" << std::endl;
         return;
     }
     file = fname.substr(0, colon_pos);
@@ -274,15 +277,15 @@ void process_file_blockpc(const char *filename) {
 
     CppFile cpp(file.c_str());
     if (!cpp.good()) {
-        std::cerr << "E: failed to open file: `" << filename << "'" << std::endl;
+        logger << error << "failed to open file: `" << filename << "'" << std::endl;
         return;
     }
 
     ConditionalBlock * block = cpp.getBlockAtPosition(filename);
 
     if (block == 0) {
-         std::cout << "I: No block at " << filename << " was found." << std::endl;
-         return;
+        logger << info << "No block at " << filename << " was found." << std::endl;
+        return;
     }
 
 
@@ -291,8 +294,8 @@ void process_file_blockpc(const char *filename) {
     try {
         defect = BlockDefectAnalyzer::analyzeBlock(block, model);
     } catch (SatCheckerError &e) {
-        std::cerr << "Couldn't process " << filename << ":" << block->getName() << ": "
-                  << e.what() << std::endl;
+        logger << error << "Couldn't process " << filename << ":" << block->getName() << ": "
+               << e.what() << std::endl;
     }
 
     // /* Get the Precondition */
@@ -304,9 +307,9 @@ void process_file_blockpc(const char *filename) {
          defect_string = defect->getSuffix() + "/" + defect->defectTypeToString();
     }
 
-    std::cout << "I: Block " << block->getName()
-              << " | Defect: " << defect_string
-              << " | Global: " << (defect != 0 ? defect->isGlobal() : 0)<< std::endl;
+    logger << info << "Block " << block->getName()
+           << " | Defect: " << defect_string
+           << " | Global: " << (defect != 0 ? defect->isGlobal() : 0)<< std::endl;
 
     std::cout << precondition << std::endl;
 }
@@ -314,7 +317,7 @@ void process_file_blockpc(const char *filename) {
 void process_file_dead_helper(const char *filename) {
     CppFile file(filename);
     if (!file.good()) {
-        std::cerr << "E: failed to open file: `" << filename << "'" << std::endl;
+        logger << error << "failed to open file: `" << filename << "'" << std::endl;
         return;
     }
 
@@ -332,8 +335,8 @@ void process_file_dead_helper(const char *filename) {
                 delete defect;
             }
         } catch (SatCheckerError &e) {
-            std::cerr << "Couldn't process " << filename << ":" << block->getName() << ": "
-                      << e.what() << std::endl;
+            logger << error << "Couldn't process " << filename << ":" << block->getName() << ": "
+                   << e.what() << std::endl;
         }
     }
 }
@@ -342,8 +345,8 @@ void process_file_dead(const char *filename) {
     boost::thread t(process_file_dead_helper, filename);
 
     if (!t.timed_join(boost::posix_time::seconds(300)))
-        std::cerr << "E: timeout passed while processing " << filename
-                  << std::endl;
+        logger << error << "timeout passed while processing " << filename
+               << std::endl;
 }
 
 
@@ -351,7 +354,7 @@ void process_file_interesting(const char *filename) {
     ConfigurationModel *model = ModelContainer::lookupMainModel();
 
     if (!model) {
-        std::cerr << "E: for finding interessting items a model must be loaded" << std::endl;
+        logger << error << "for finding interessting items a model must be loaded" << std::endl;
         return;
     }
 
@@ -384,7 +387,7 @@ void process_file_symbolpc(const char *filename) {
     ConfigurationModel *model = ModelContainer::lookupMainModel();
 
     if (!model) {
-        std::cerr << "E: for symbolpc models must be loaded" << std::endl;
+        logger << error << "for symbolpc models must be loaded" << std::endl;
         return;
     }
 
@@ -392,7 +395,7 @@ void process_file_symbolpc(const char *filename) {
 
     std::set<std::string> missingItems;
 
-    std::cout << "I: Symbol Precondition for `" << filename << "'" << std::endl;
+    logger << info << "Symbol Precondition for `" << filename << "'" << std::endl;
 
 
     /* Find all items that are related to the given item */
@@ -452,11 +455,13 @@ void wait_for_forked_child(pid_t new_pid, int threads = 1, const char *argument 
                 arguments.erase(pid);
             } else {
                 process_stats.failed ++;
-                std::cout << "E: Process (" << arguments[pid] << ") failed with exitcode " << WEXITSTATUS(state) << std::endl;
+                logger << error << "Process (" << arguments[pid]
+                       << ") failed with exitcode " << WEXITSTATUS(state) << std::endl;
             }
         } else if (WIFSIGNALED(state)) {
             process_stats.signaled++;
-            std::cout << "E: Process (" << arguments[pid] << ") failed with signal " << WTERMSIG(state) << std::endl;
+            logger << error << "Process (" << arguments[pid]
+                   << ") failed with signal " << WTERMSIG(state) << std::endl;
         } else {
             continue;
         }
@@ -465,18 +470,17 @@ void wait_for_forked_child(pid_t new_pid, int threads = 1, const char *argument 
 
     if (print_stats) {
         /* Shutdown phase */
-        std::cout << "I: Sucessful processed:  " << process_stats.ok << std::endl;
-        std::cout << "I: Failed with exitcode: " << process_stats.failed << std::endl;
-        std::cout << "I: Failed with signal:   " << process_stats.signaled << std::endl;
+        logger << info << "Sucessful processed:  " << process_stats.ok << std::endl;
+        logger << info << "Failed with exitcode: " << process_stats.failed << std::endl;
+        logger << info << "Failed with signal:   " << process_stats.signaled << std::endl;
         for (std::map<pid_t, const char *>::const_iterator it = arguments.begin() ; it != arguments.end(); it++ )
-            std::cout << "I: Failed file: " << (*it).second << endl;
+            logger << info << "Failed file: " << (*it).second << endl;
     }
 }
 
 
 int main (int argc, char ** argv) {
     int opt;
-    bool quiet(false);
     char *worklist = NULL;
     char *whitelist = NULL;
 
@@ -486,6 +490,13 @@ int main (int argc, char ** argv) {
     /* Default is dead/undead analysis */
     std::string process_mode = "dead";
     process_file_cb_t process_file = process_file_dead;
+
+    /* Standard Logging output is standard out,
+       the loglevel (threshold) is LOG_INFO
+       the default Loglevel is LOG_INFO
+    */
+    logger.init(std::cout, Logging::LOG_INFO, Logging::LOG_INFO);
+    int loglevel = Logging::LOG_INFO;
 
     /* Command line structure:
        - Model Options:
@@ -498,7 +509,7 @@ int main (int argc, char ** argv) {
     */
     coverageOutputMode = COVERAGE_MODE_KCONFIG;
 
-    while ((opt = getopt(argc, argv, "cb:M:m:t:w:j:O:C:I:vhq")) != -1) {
+    while ((opt = getopt(argc, argv, "cb:M:m:t:w:j:O:C:I:Vhvq")) != -1) {
         switch (opt) {
         case 'w':
             whitelist = strdup(optarg);
@@ -526,8 +537,8 @@ int main (int argc, char ** argv) {
                     coverage_exec_cmd = &optarg[5];
             }
             else
-                std::cerr << "WARNING, mode " << optarg << " is unknown, using 'kconfig' instead"
-                          << std::endl;
+                logger << warn << "mode " << optarg << " is unknown, using 'kconfig' instead"
+                       << std::endl;
             break;
         case 'C':
             if (0 == strcmp(optarg, "simple"))
@@ -535,8 +546,8 @@ int main (int argc, char ** argv) {
             else if (0 == strcmp(optarg, "min"))
                 coverageOperationMode = COVERAGE_OP_MINIMIZE;
             else
-                std::cerr << "WARNING, mode " << optarg << " is unknown, using 'simple' instead"
-                          << std::endl;
+                logger << warn << "mode " << optarg << " is unknown, using 'simple' instead"
+                       << std::endl;
             break;
         case 't':
             threads = strtol(optarg, (char **)0, 10);
@@ -546,7 +557,7 @@ int main (int argc, char ** argv) {
                 exit(EXIT_FAILURE);
             }
             if (threads < 1) {
-                std::cerr << "WARNING: Invalid numbers of threads, using 1 instead." << std::endl;
+                logger << warn << "Invalid numbers of threads, using 1 instead." << std::endl;
                 threads = 1;
             }
             break;
@@ -571,23 +582,30 @@ int main (int argc, char ** argv) {
             PumaConditionalBlockBuilder::addIncludePath(optarg);
             break;
         case 'h':
-            std::cout << "undertaker " << version << std::endl;
+            logger << info << "undertaker " << version << std::endl;
             usage(std::cout, NULL);
             exit(0);
             break;
-        case 'v':
-            std::cout << "undertaker " << version << std::endl;
+        case 'V':
+            logger << info << "undertaker " << version << std::endl;
             exit(0);
             break;
         case 'q':
-            quiet = true;
+            loglevel = loglevel + 10;
+            logger.setLogLevel(loglevel);
+            break;
+        case 'v':
+            loglevel = loglevel - 10;
+            if (loglevel < 0)
+                loglevel = Logging::LOG_EVERYTHING;
+            logger.setLogLevel(loglevel);
+            break;
         default:
             break;
         }
     }
 
-    if (!quiet)
-        std::cout << "undertaker " << version << std::endl;
+    logger << debug << "undertaker " << version << std::endl;
 
     if (!worklist && optind >= argc) {
         usage(std::cout, "please specify a file to scan or a worklist");
@@ -605,9 +623,9 @@ int main (int argc, char ** argv) {
         KconfigWhitelist *wl = KconfigWhitelist::getInstance();
         int n = wl->loadWhitelist(whitelist);
         if (n >= 0) {
-            std::cout << "I: loaded " << n << " items to whitelist" << std::endl;
+            logger << info << "loaded " << n << " items to whitelist" << std::endl;
         } else {
-            std::cout << "E: couldn't load whitelist" << std::endl;
+            logger << error << "couldn't load whitelist" << std::endl;
             exit(-1);
         }
         free(whitelist);
@@ -676,7 +694,7 @@ int main (int argc, char ** argv) {
                 } else { /* Change working mode */
                     process_file_cb_t new_function = parse_job_argument(new_mode.c_str());
                     if (!new_function) {
-                        std::cerr << "Invalid new working mode: " << new_mode << std::endl;
+                        logger << error << "Invalid new working mode: " << new_mode << std::endl;
                         continue;
                     }
                     /* now change the pointer and the working mode */
@@ -695,7 +713,7 @@ int main (int argc, char ** argv) {
                 process_file(workfiles[i].c_str());
                 exit(EXIT_SUCCESS);
             } else if (pid < 0) {
-                std::cerr << "E: forking failed. Exiting." << std::endl;
+                logger << error << "forking failed. Exiting." << std::endl;
                 exit(EXIT_FAILURE);
             } else { /* Father process */
                 wait_for_forked_child(pid, threads, workfiles[i].c_str());

undertaker/BlockDefectAnalyzer.cpp
@@ -27,6 +27,7 @@
 #include "BlockDefectAnalyzer.h"
 #include "SatChecker.h"
 #include "ModelContainer.h"
+#include "Logging.h"
 
 
 void BlockDefectAnalyzer::markOk(const std::string &arch) {
@@ -211,10 +212,10 @@ bool DeadBlockDefect::writeReportToFile() const {
     std::ofstream out(filename.c_str());
 
     if (!out.good()) {
-        std::cerr << "failed to open " << filename << " for writing " << std::endl;
+        logger << error << "failed to open " << filename << " for writing " << std::endl;
         return false;
     } else {
-        std::cout << "I: creating " << filename << std::endl;
+        logger << info << "creating " << filename << std::endl;
         out << "#" << _cb->getName() << ":" 
             << _cb->filename() << ":" << _cb->lineStart() << ":" << _cb->colStart() << ":"
             << _cb->filename() << ":" << _cb->lineEnd() << ":" << _cb->colEnd() << ":"

undertaker/ConfigurationModel.cpp
@@ -23,6 +23,7 @@
 #include "ConfigurationModel.h"
 #include "KconfigWhitelist.h"
 #include "StringJoiner.h"
+#include "Logging.h"
 
 #include <boost/algorithm/string/predicate.hpp>
 #include <cassert>
@@ -43,7 +44,8 @@ ConfigurationModel::ConfigurationModel(std::string name, std::istream *in, std::
     configuration_space_regex = _model->getMetaValue("CONFIGURATION_SPACE_REGEX");
 
     if (configuration_space_regex != NULL && configuration_space_regex->size() > 0) {
-        std::cout << "I: Set configuration space regex to '" << configuration_space_regex->front() << "'" << std::endl;
+        logger << info << "Set configuration space regex to '"
+               << configuration_space_regex->front() << "'" << std::endl;
         _inConfigurationSpace_regexp = boost::regex(configuration_space_regex->front(), boost::regex::perl);
     } else
         _inConfigurationSpace_regexp = boost::regex("^CONFIG_[^ ]+$", boost::regex::perl);

undertaker/CoverageAnalyzer.cpp
@@ -20,6 +20,7 @@
 
 #include "StringJoiner.h"
 #include "CoverageAnalyzer.h"
+#include "Logging.h"
 
 /* c'tor */
 CoverageAnalyzer::CoverageAnalyzer(CppFile *file) : file(file) {};
@@ -32,7 +33,8 @@ std::string CoverageAnalyzer::baseFileExpression(const ConfigurationModel *model
         const std::string magic("ALWAYS_ON");
         always_on = model->getMetaValue(magic);
         if (always_on && !blocks) {
-            std::cout << "I: " << always_on->size() << " Items have been forcefully set" << std::endl;
+            logger << info << always_on->size()
+                   << " Items have been forcefully set" << std::endl;
         }
     }
 
@@ -136,8 +138,8 @@ std::list<SatChecker::AssignmentMap> SimpleCoverageAnalyzer::blockCoverage(Confi
             }
         }
     } catch (SatCheckerError &e) {
-        std::cerr << "Couldn't process " << file->getFilename() << ": "
-                  << e.what() << std::endl;
+        logger << error << "Couldn't process " << file->getFilename() << ": "
+               << e.what() << std::endl;
     }
 
     return ret;
@@ -236,11 +238,11 @@ std::list<SatChecker::AssignmentMap> MinimizeCoverageAnalyzer::blockCoverage(Con
             assert(configuration.size() == 0);
         }
     } catch (SatCheckerError &e) {
-        std::cerr << "Couldn't process " << file->getFilename() << ": "
-                  << e.what() << std::endl;
+        logger << error << "Couldn't process " << file->getFilename() << ": "
+               << e.what() << std::endl;
     } catch (std::bad_alloc &e) {
-        std::cerr << "Couldn't process " << file->getFilename() << ": "
-                  << e.what() << std::endl;
+        logger << error << "Couldn't process " << file->getFilename() << ": "
+               << e.what() << std::endl;
     }
     return ret;
 }

undertaker/Logging.cpp
@@ -1,0 +1,72 @@
+/*
+ *   undertaker - analyze preprocessor blocks in code
+ *
+ * Copyright (C) 2011 Christian Dietrich <christian.dietrich@informatik.uni-erlangen.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "Logging.h"
+
+Logging logger; // Global log object
+
+Logging::Logging() {
+    init();
+}
+
+void Logging::log(int level, std::string input) {
+    if (level >= loglevel)
+        (*out) << logPrefix(level) << ": " << input << std::endl;
+};
+
+void Logging::debug(std::string input) { log(LOG_DEBUG,   input); };
+void Logging::info(std::string input)  { log(LOG_INFO,    input); };
+void Logging::warn(std::string input)  { log(LOG_WARNING, input); };
+void Logging::error(std::string input) { log(LOG_ERROR,   input); };
+
+void Logging::init(std::ostream &out_stream, Logging::LogLevel _loglevel,
+                   Logging::LogLevel _default_loglevel) {
+    out = &out_stream;
+    setLogLevel(_loglevel);
+    setDefaultLogLevel(_default_loglevel);
+}
+
+std::string Logging::logPrefix(int level) {
+    if (level <= LOG_DEBUG) return "D";
+    if (level <= LOG_INFO) return "I";
+    if (level <= LOG_WARNING) return "W";
+    return "E";
+}
+
+Logging & Logging::operator<< (std::ostream& (*f)(std::ostream &)) {
+    std::stringstream s;
+    s << f;
+    if (s.str() == "\n") {
+        log(actual_level, buffer.str());
+        buffer.str("");
+        actual_level = default_level;
+    } else {
+        buffer << f;
+    }
+    return *this;
+}
+
+Logging & Logging::operator<< (Logging& (*f)(Logging &)) {
+    return (f(*this));
+}
+
+Logging& debug(Logging &l) { l.setActualLogLevel(Logging::LOG_DEBUG); return l; };
+Logging& info(Logging &l)  { l.setActualLogLevel(Logging::LOG_INFO);  return l; };
+Logging& warn(Logging &l)  { l.setActualLogLevel(Logging::LOG_WARNING);  return l; };
+Logging& error(Logging &l) { l.setActualLogLevel(Logging::LOG_ERROR);  return l; };

undertaker/Logging.h
@@ -1,0 +1,79 @@
+/*
+ *   undertaker - analyze preprocessor blocks in code
+ *
+ * Copyright (C) 2011 Christian Dietrich <christian.dietrich@informatik.uni-erlangen.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <iostream>
+#include <cstdlib>
+#include <sstream>
+
+#ifndef _LOGGING_H_
+#define _LOGGING_H_
+
+class Logging {
+public:
+    Logging();
+
+    enum LogLevel {
+        LOG_EVERYTHING = 0,
+        LOG_DEBUG = 10,
+        LOG_INFO = 20,
+        LOG_WARNING = 30,
+        LOG_ERROR = 40,
+    };
+    void log(int level, std::string input);
+
+    void debug(std::string input);
+    void info(std::string input);
+    void warn(std::string input);
+    void error(std::string input);
+
+    void setLogLevel(int l) { loglevel = l; };
+    void setDefaultLogLevel(int l) { default_level = l; };
+    void setActualLogLevel(int l) { actual_level = l; };
+
+
+    void init(std::ostream &out_stream=std::cerr,
+              LogLevel _loglevel = LOG_WARNING,
+              LogLevel _default_loglevel = LOG_INFO);
+
+    /* Catchall for all other stream operator<< arguments */
+    template<typename T>
+    Logging & operator<< (const T &t) {
+        buffer << t;
+        return *this;
+    };
+
+    Logging & operator<< (std::ostream& (*f)(std::ostream &));
+    Logging & operator<< (Logging& (*f)(Logging &));
+
+private:
+    std::string logPrefix(int level);
+    int loglevel, default_level, actual_level;
+    std::ostream *out;
+    std::stringstream buffer;
+};
+
+extern Logging logger;
+
+Logging& debug(Logging &l);
+Logging& info(Logging &l);
+Logging& warn(Logging &l);
+Logging& error(Logging &l);
+
+
+#endif /* _LOGGING_H_ */

undertaker/Makefile
@@ -17,7 +17,8 @@ LDLIBS = libparser.a -lstdc++ $(BOOST_LIBS) -lpicosat -lboost_wave-mt -lPuma $(L
 
 PARSEROBJ = RsfReader.o KconfigWhitelist.o ConfigurationModel.o\
 		ModelContainer.o SatChecker.o BlockDefectAnalyzer.o\
-		ConditionalBlock.o CoverageAnalyzer.o PumaConditionalBlock.o
+		ConditionalBlock.o CoverageAnalyzer.o PumaConditionalBlock.o \
+		Logging.o
 
 DEPFILES:=$(patsubst %.o,%.d,$(PARSEROBJ)) undertaker.d
 

commit 00d698fa8549cdd8134484fdbcb9ca871f042a53

undertaker: unbreak help text
Commit 387c5e160ef2a98dd0f926762b54a391357e552b introduced additional
commandline switches, but the formatting was wrong. This commit restores
readability again.

Change-Id: Ice2312207c21883f7c3ce5aa7b6ba06d22134822

undertaker/undertaker.cpp
@@ -76,12 +76,12 @@ static const char *coverage_exec_cmd = "cat";
 void usage(std::ostream &out, const char *error) {
     if (error)
         out << error << std::endl << std::endl;
-    out << "`undertaker' does analysis on conditinal C code.\n\n";
+    out << "`undertaker' analyzes conditional C code with #ifdefs.\n\n";
     out << "Usage: undertaker [OPTIONS] <file..>\n";
     out << "\nOptions:\n";
-    out << "  -V  print version information";
-    out << "  -v  increase the log level (more verbose)";
-    out << "  -q  decrease the log level (less verbose)";
+    out << "  -V  print version information\n";
+    out << "  -v  increase the log level (more verbose)\n";
+    out << "  -q  decrease the log level (less verbose)\n";
     out << "  -m  specify the model(s) (directory or file)\n";
     out << "  -M  specify the main model\n";
     out << "  -w  specify a whitelist\n";