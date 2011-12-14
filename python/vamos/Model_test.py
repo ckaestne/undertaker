ckaestne/undertaker

commit 0f1f73981791df72ac5642294e212c790541e195

undertaker-calc-coverage: factor out common functionality
This package will be shared among future python utilities and programs.

Change-Id: I0df6973e3f090c9c1a4f15f181bd56a6b3b43053

python/undertaker-calc-coverage
@@ -18,6 +18,8 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
+from vamos.tools import execute, setup_logging
+
 import os
 import shutil
 import sys
@@ -58,21 +60,6 @@ class ExpansionSanityCheckError(ExpansionError):
 
 class PredatorError(RuntimeError):
     pass
-
-def execute(command, echo=True):
-    """
-    executes 'command' in a shell
-
-    returns a tuple with
-     1. the command's standard output as list of lines
-     2. the exitcode
-    """
-
-    if echo:
-        logging.debug("executing: %s" % command)
-    p = Popen(command, stdout=PIPE, stderr=STDOUT, shell=True)
-    (stdout, stderr) = p.communicate()
-    return (stdout.rsplit('\n'), p.returncode)
 
 def switch_config(config):
     """
@@ -460,14 +447,7 @@ def main():
                       help="call sparse on each configuration")
 
     (options, args) = parser.parse_args()
-
-    log_level = logging.WARNING # default
-    if options.verbose == 1:
-        log_level = logging.INFO
-    elif options.verbose >= 2:
-        log_level = logging.DEBUG
-
-    logging.basicConfig(level=log_level)
+    setup_logging(options.verbose)
 
     if options.model and os.path.exists(options.model):
         with open(options.model) as fd:

python/vamos/tools.py
@@ -1,0 +1,53 @@
+#
+#   vamos - common auxiliary functionality
+#
+# Copyright (C) 2011 Christian Dietrich <christian.dietrich@informatik.uni-erlangen.de>
+# Copyright (C) 2011 Reinhard Tartler <tartler@informatik.uni-erlangen.de>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+import logging
+import os
+
+from subprocess import *
+
+def setup_logging(log_level):
+    """ setup the logging module with the given log_level """
+
+    l = logging.WARNING # default
+    if log_level == 1:
+        l = logging.INFO
+    elif log_level >= 2:
+        l = logging.DEBUG
+
+    logging.basicConfig(level=l)
+
+
+def execute(command, echo=True):
+    """
+    executes 'command' in a shell
+
+    returns a tuple with
+     1. the command's standard output as list of lines
+     2. the exitcode
+    """
+    os.environ["LC_ALL"] = "C"
+
+    if echo:
+        logging.debug("executing: " + command)
+    p = Popen(command, stdout=PIPE, stderr=STDOUT, shell=True)
+    (stdout, _)  = p.communicate() # ignore stderr
+    if len(stdout) > 0 and stdout[-1] == '\n':
+        stdout = stdout[:-1]
+    return (stdout.__str__().rsplit('\n'), p.returncode)

setup.py
@@ -6,7 +6,7 @@ with open('VERSION') as f:
 
 setup (name='vamos',
        version=version,
-       package_dir={'vamos' : 'python/vamos/',
+       package_dir={'vamos' : 'python/vamos',
                     'vamos.rsf2model' : 'python/vamos/rsf2model',
                     },
        packages=['vamos',

commit cf306cb2278e854036929e91e4cd75407265bc14

undertaker-calc-coverage: unbreak and reduce code duplication
This utility silently got broken after git commit
0f1f73981791df72ac5642294e212c790541e195 and the python search path
related changes. The existing Modelreader implementation is extended by
a field "always_on_items" so that the class can be re-used in
undertaker-calc-coverage.

Additionally some minor cleanups have been added to
undertaker-calc-coverage and the model class

Change-Id: Ibe953656397dacb53d67c15007f8ab5b1caf2856

python/undertaker-calc-coverage
@@ -18,35 +18,24 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-from vamos.tools import execute, setup_logging
-
-import os
-import shutil
 import sys
+from os.path import join, dirname
+
+sys.path = [join(dirname(sys.path[0]), 'lib', 'python%d.%d' % \
+                (sys.version_info[0], sys.version_info[1]), 'site-packages')] \
+                + sys.path
+
+from vamos.tools import execute, setup_logging
+from vamos.model import Model
+from vamos.golem.kbuild import apply_configuration
+
+import os.path
+import shutil
 import glob
 import re
-import shlex
 import logging
 from subprocess import Popen, STDOUT, PIPE
 from optparse import OptionParser
-
-class ModelReader(dict):
-    def __init__(self, fd):
-        dict.__init__(self, fd)
-
-        self.meta_info = dict()
-
-        for l in fd:
-            if l.startswith('I: '):
-                continue
-            if l.startswith('UNDERTAKER_SET'):
-                row = shlex.split(l)[1:]
-                if len(row) > 0:
-                    self.meta_info[row[0]] = row[1:]
-                continue
-            row = shlex.split(l)
-            if len(row) > 0:
-                self[row[0]] = row[1:]
 
 model = None
 
@@ -67,14 +56,15 @@ def switch_config(config):
     """
     switches the current tree to the given config. throws an exception in case make fails
     """
-    execute("find include -name autoconf.h -exec rm {} +", echo=True)
+    execute("find include -name autoconf.h -delete", echo=True)
 
     (output, returncode) = execute("make %s" % config, echo=False)
     if returncode != 0:
         raise RuntimeError("Failed to switch to config %s" % config)
-    (_, returncode2) = execute("make silentoldconfig", echo=False)
-    if returncode2 != 0:
-        raise RuntimeError("silentoldconfig failed while switching to config %s" % config)
+
+    # sanity check
+    apply_configuration()
+
     return output
 
 def switch_config_path(filename):
@@ -270,7 +260,7 @@ def verify_config(partial_config):
         for l in partial_configf:
             if l.startswith('CONFIG_'):
                 (item, value) = l.split('=')
-                if model and item in model.meta_info['ALWAYS_ON']:
+                if model and item in model.always_on_items:
                     continue
                 if item in config_whitelist:
                     continue
@@ -282,7 +272,7 @@ def verify_config(partial_config):
             if l.startswith('CONFIG_'):
                 (item, value) = l.split('=')
                 found_items[item] = value.rstrip()
-                if model and item in model.meta_info['ALWAYS_ON']:
+                if model and item in model.always_on_items:
                     continue
                 if (item in value_errors):
                     if (value_errors[item] == value.rstrip()):
@@ -359,9 +349,9 @@ def get_loc_coverage(filename):
     """
 
     cmd = "grep -v -E '^\s*#\s*include' %s | cpp -include %s" % (filename, find_autoconf())
-    (lines, rc) = execute(cmd, echo=True)
-    if rc != 0:
-        raise RuntimeError("Calculating loc coverage failed, rc: %d" % rc)
+    (lines, _) = execute(cmd, echo=True)
+    # we ignore the exitcode here as we are not interested in failing
+    # for #error directives and similar.
     return len(lines)
 
 
@@ -403,12 +393,13 @@ def vamos_coverage(filename, results, options):
                 mismatches = list()
                 for c in value_errors.keys():
                     mismatches.append("%s != %s" % (c, value_errors[c]))
-                logging.debug("".join(mismatches))
+                logging.debug(", ".join(mismatches))
         except ValueError:
             logging.warning("Failure to analyze partial config expansion, continuing anyways")
 
         results[filename][config]['value_errors'] = value_errors
         results[filename][config]['selected_items'] = selected_items(config)
+        results[filename][config]['loc_coverage'] = get_loc_coverage(filename)
 
         if options.run_sparse:
             (warnings, errors) = run_sparse_on_config(filename, config)
@@ -464,25 +455,15 @@ def parse_options():
     setup_logging(options.verbose)
 
     if options.model and os.path.exists(options.model):
-        with open(options.model) as fd:
-            global model
-            model = ModelReader(fd)
-            logging.info("Loaded %d options from Model %s", (len(model), options.model))
-            logging.info("%d items are always on", len(model.meta_info['ALWAYS_ON']))
+        global model
+        model = Model(options.model)
+        logging.info("Loaded %d options from Model %s", len(model), options.model)
+        logging.info("%d items are always on", len(model.always_on_items))
 
     if len(args) == 0:
         logging.critical("please specify a worklist")
         sys.exit(1)
 
-    worklist = args[0]
-    try:
-        with open(worklist) as f:
-            filenames = [x.strip() for x in f.readlines()]
-    except IOError as error:
-        logging.critical("failed to open worklist: " + error.__str__())
-        sys.exit(1)
-
-    logging.debug("Worklist contains %d items", len(filenames))
     return parser.parse_args()
 
 def do_work(filenames, options):
@@ -538,12 +519,15 @@ def do_work(filenames, options):
                         print "selected_items: %d"  % results[filename][config]['selected_items']
                         print "runtime: 0.0"
                         print "covered_blocks: %d"  % len(results[filename][config]['covered_blocks'])
+                        print "loc_coverage: %d" % results[filename][config]['loc_coverage']
+
+                        if options.run_sparse:
+                            print "sparse_warnings: %d" % results[filename][config]['sparse_warnings']
+                            print "sparse_errors: %d"   % results[filename][config]['sparse_errors']
+
                     except KeyError:
                         pass
 
-                    if options.run_sparse:
-                        print "sparse_warnings: %d" % results[filename][config]['sparse_warnings']
-                        print "sparse_errors: %d"   % results[filename][config]['sparse_errors']
 
         except PredatorError as error:
             continue
@@ -555,15 +539,6 @@ def do_work(filenames, options):
 def main():
     (options, args) = parse_options()
     setup_logging(options.verbose)
-
-    if options.model and os.path.exists(options.model):
-        with open(options.model) as fd:
-            global model
-            model = ModelReader(fd)
-            logging.info("Loaded %d options from Model %s",
-                         len(model), options.model)
-            logging.info("%d items are always on",
-                         len(model.meta_info['ALWAYS_ON']))
 
     if len(args) == 0:
         logging.critical("please specify a worklist")
@@ -577,9 +552,13 @@ def main():
         logging.critical("failed to open worklist: " + error.__str__())
         sys.exit(1)
 
-    logging.debug("Worklist contains %d items", len(filenames))
-
-    do_work(worklist, filenames)
+    if len(filenames) > 0:
+        logging.debug("Worklist contains %d items", len(filenames))
+        do_work(filenames, options)
+        sys.exit(0)
+    else:
+        logging.error("Worklist contains no work items!")
+        sys.exit(1)
 
 if __name__ == "__main__":
     main()

python/vamos/model.py
@@ -21,14 +21,30 @@ class Model(dict):
     def __init__(self, path):
         dict.__init__(self)
         self.path = path
-        fd = open(path)
+        self.always_on_items = set()
+
+        with open(path) as fd:
+            self.parse(fd)
+
+        if path.endswith(".model"):
+            rsf = path[:-len(".model")] + ".rsf"
+            try:
+                with open(rsf) as f:
+                    self.rsf = RsfReader(f)
+            except IOError:
+                self.rsf = None
+        else:
+            self.rsf = None
+
+    def parse(self, fd):
         for line in fd.readlines():
             line = line.strip()
             if line.startswith("UNDERTAKER_SET ALWAYS_ON"):
                 line = line.split(" ")[2:]
-                line = [ l.strip(" \t\"") for l in line]
-                for item in line:
+                always_on_items = [ l.strip(" \t\"") for l in line]
+                for item in always_on_items:
                     self[item] = None
+                    self.always_on_items.add(item)
                 continue
             elif line.startswith("I:") or line.startswith("UNDERTAKER_SET"):
                 continue
@@ -37,15 +53,6 @@ class Model(dict):
                 self[line[0]] = None
             elif len(line) == 2:
                 self[line[0]] = line[1].strip(" \"\t\n")
-
-        if path.endswith(".model"):
-            rsf = path[:-len(".model")] + ".rsf"
-            try:
-                self.rsf = RsfReader(open(rsf))
-            except IOError:
-                self.rsf = None
-        else:
-            self.rsf = None
 
     def mentioned_items(self, key):
         """Return list of mentioned items of the key's implications"""

python/vamos/Model_test.py
@@ -44,6 +44,14 @@ CONFIG_C_MODULE "!CONFIG_C && CONFIG_MODULE"
         self.assertEqual(model["CONFIG_B"], "CONFIG_A")
         self.assertEqual(model["CONFIG_C"], "!CONFIG_C_MODULE")
 
+    def test_symbol_always_on(self):
+        model = Model(self.model_file.name)
+        for symbol in ("CONFIG_ALWAYS_ON", "CONFIG_BARFOO"):
+            self.assertTrue(symbol in model.always_on_items, msg="%s not in model" % symbol)
+
+        for symbol in ("CONFIG_A", "CONFIG_B", "CONFIG_C", "CONFIG_MODULE"):
+            self.assertFalse(symbol in model.always_on_items, msg="%s not in model" % symbol)
+
     def test_leaf_features(self):
         model = Model(self.model_file.name)
         self.assertEqual(set(model.leaf_features()),set(['CONFIG_X', 'CONFIG_ALWAYS_ON', 'CONFIG_BARFOO', 'CONFIG_B']) )