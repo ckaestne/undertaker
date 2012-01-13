ckaestne/undertaker

commit 99e897bdde86ef295fa874713e8b5ced70d7defb

undertaker-linux-tree: implement kconfig item statistics
Introduce (a number of) statistics about variability usage in Linux.
Also extend the undertaker-scan-head script to call the statistics.

This change requires two smaller presentational changes in golem.

In order to assist the statistics, cppsym mode supresses irrelevant
noise like rewritten CPP symbols symbols, operators, etc.

Change-Id: I7352a4493956d4761b85224c257ab55bdd1e53d0

python/vamos/golem/kbuild.py
@@ -75,10 +75,15 @@ def files_for_current_configuration(how=False):
             continue
         try:
             if (how):
-                l = line
+                objfile = line
             else:
-                l = line.split()[0]
-            files.add(l)
+                objfile = line.split()[0]
+            # try to guess the source filename
+            sourcefile = objfile[:-2] + '.c'
+            if os.path.exists(sourcefile):
+                files.add(sourcefile)
+            else:
+                logging.warning("Failed to guess source file for %s", objfile)
         except IndexError:
             raise RuntimeError("Failed to parse line '%s'" % line)
 

python/golem
@@ -70,7 +70,7 @@ def main():
         variables = determine_buildsystem_variables()
         logging.info("Detected %d Kconfig variables in Makefiles", len(variables))
         for v in variables:
-            print v
+            print 'CONFIG_' + v
         sys.exit(0)
 
     if opts.do_list:

undertaker/undertaker-linux-tree
@@ -7,7 +7,10 @@
 # before, in order to create the models.
 #
 
-while getopts :t:m:a:cvh OPT; do
+# scan for deads by default
+MODE="scan-deads"
+
+while getopts :t:m:a:csvh OPT; do
     case $OPT in
         m)
             MODELS="$OPTARG"
@@ -19,16 +22,19 @@ while getopts :t:m:a:cvh OPT; do
             PROCESSORS="$OPTARG"
             ;;
         c)
-            COVERAGE=t
+            MODE="calc-coverage"
+            ;;
+        s)
+            MODE="feature-statistics"
             ;;
         v)
             echo "undertaker-linux-tree"
             exit
             ;;
         h)
-            echo "\`undertaker-linux-tree' runs the undertaker a whole linux-tree"
+            echo "\`undertaker-linux-tree' drives the undertaker over a whole linux-tree"
             echo
-            echo "Usage: ${0##*/} [-m DIR] [-a ARCH] [-t PROCS] [-c]"
+            echo "Usage: ${0##*/} [-m DIR] [-a ARCH] [-t PROCS] [-c|-s]"
             echo " -m <modeldir>  Specify the directory for the models"
             echo "           (default: models)"
             echo " -a <arch>  Default architecture to check for"
@@ -36,6 +42,7 @@ while getopts :t:m:a:cvh OPT; do
             echo " -t <count>   Number of analyzing processes"
             echo "        (default: _NPROCESSORS_ONLN)"
             echo " -c  Do coverage analysis instead of dead block search"
+            echo " -s  Do feature statistics instead of dead block search"
             exit
             ;;
     esac
@@ -64,7 +71,7 @@ if ! ls "$MODELS"/*.model >/dev/null 2>&1; then
     exit
 fi
 
-if [ -n "$COVERAGE" ]; then
+if [ "$MODE" = "calc-coverage" ]; then
     find -type f -name '*.c' \
         ! -regex '^./tools.*' ! -regex '^./Documentation.*' ! -regex '^./scripts.*' \
         -exec grep -q -E '^#else' {} \; -print | shuf > undertaker-coverage-worklist
@@ -99,7 +106,7 @@ if [ -n "$COVERAGE" ]; then
     fi
 fi
 
-else
+if [ "$MODE" = "scan-deads" ]; then
     find -type f -name "*.[hcS]" \
         ! -regex '^./tools.*' ! -regex '^./Documentation.*' ! -regex '^./scripts.*' \
         -exec grep -q -E '^#else' {} \; -print | shuf > undertaker-worklist
@@ -110,4 +117,66 @@ else
     echo "Analyzing $(wc -l < undertaker-worklist) files with $PROCESSORS threads."
     undertaker -v -t "$PROCESSORS" -b undertaker-worklist -m "$MODELS" -M "$DEFAULT_ARCH"
     printf "\n\nFound %s global defects\n" "$(find . -name '*dead'| grep globally | wc -l)"
+    exit 0
+fi
+
+do_archstat () {
+    CONFIG=$1
+    ARCH=$2
+    CROSS_COMPILE=
+    export ARCH CROSS_COMPILE
+
+    if ! make $CONFIG >/dev/null 2>&1; then
+        # some strange architectures (e.g., h8300) are just utterly broken. skip them
+        echo "Skipping $ARCH ($CONFIG)"
+        return
+    fi
+    if ! golem -l >undertaker-stat/$CONFIG-$ARCH.list  2>undertaker-stat/$CONFIG-$ARCH.failed; then
+        echo "golem -l failed on $ARCH with $CONFIG, skipping"
+        return
+    fi
+    echo -n "Architecture $ARCH uses $(cat undertaker-stat/$CONFIG-$ARCH.list | wc -l) source files for $CONFIG"
+    echo " (plus possibly $(grep -c 'Failed to guess' undertaker-stat/$CONFIG-$ARCH.failed) additional files)"
+    if ! undertaker -j cppsym -t $PROCESSORS -b undertaker-stat/$CONFIG-$ARCH.list -m models/$ARCH.model |
+         grep -v -E '(MISSING|NON_KCONFIG)' | sort |
+         uniq >undertaker-stat/$CONFIG-$ARCH.cppsym 2>undertaker-stat/$CONFIG-$ARCH.cppsym-errors; then
+        echo "Failed to extract CPP symbols for $ARCH with $CONFIG:"
+        cat undertaker-stat/$CONFIG-$ARCH.cppsym-errors
+    fi
+    if ! test -s undertaker-stat/$CONFIG-$ARCH.cppsym-errors; then
+        rm -f undertaker-stat/$CONFIG-$ARCH.cppsym-errors
+    fi
+}
+
+if [ "$MODE" = "feature-statistics" ]; then
+    allmodels="$(ls models/*.model)"
+    allrsfs="$(ls models/*.rsf)"
+    mkdir -p undertaker-stat
+
+    golem -o | sort -u > undertaker-kbuild-variables
+    echo "Found $(cat undertaker-kbuild-variables | wc -l) configuration variables mentioned in Makefiles"
+
+    find -type f -name "*.[hcS]" \
+        ! -regex '^./tools.*' ! -regex '^./Documentation.*' ! -regex '^./scripts.*' \
+        -exec grep -q -E '^#else' {} \; -print | shuf > undertaker-worklist
+
+    if undertaker -j cppsym -t $PROCESSORS -b undertaker-worklist \
+        >undertaker-all-cppsym.raw 2>undertaker-cppsym.errors; then
+        sort -u undertaker-all-cppsym.raw | grep -v -E '^(I|E): ' >undertaker-all-cppsym
+        echo "Found $(cat undertaker-all-cppsym|wc -l) CPP symbols."
+    else
+        echo "checking for cppsym errors failed, check the error log:"
+        cat undertaker-cppsym.errors
+    fi
+    if ! test -s undertaker-cppsym.errors; then
+        rm -f undertaker-cppsym.errors
+    fi
+
+    for m in $allmodels; do
+        ARCH=$(basename $m .model)
+        do_archstat allnoconfig $ARCH
+        do_archstat allyesconfig $ARCH
+        do_archstat allmodconfig $ARCH
+    done
+    exit 0
 fi

undertaker/undertaker-scan-head
@@ -100,6 +100,7 @@ deadlist="${resultdir}/${version}-deads.txt"
 
 echo "Scanning version ${version}, logging to ${report}"
 
+
 echo "Step 0: Extract Kconfig feature dependencies"
 if $TIME -v -o kconfigdump.stats -- undertaker-kconfigdump  >"$report" \
       2>kconfigdump-error-output.txt; then
@@ -143,3 +144,19 @@ fi
 if ! test -s undertaker-coverage-errors.txt; then
     rm -f undertaker-coverage-errors.txt
 fi
+
+
+echo "Step 3: CPP symbol statistics"
+if $TIME -v -o undertaker-cppsym.stats -- undertaker-linux-tree -t ${PROCESSORS} -s \
+      2>undertaker-cppsym-stat-errors.txt; then
+    cat undertaker-cppsym.stats
+    echo "CPP symbols statistics done, check undertaker-stat/*"
+else
+    echo "CPP symbols statistics failed with exit code $?. check the error logfiles."
+    cat undertaker-cppsym-stat-errors.txt
+fi
+if ! test -s undertaker-cppsym-stat-errors.txt; then
+    rm -rf undertaker-cppsym-stat-errors.txt
+fi
+
+exit 0

undertaker/undertaker.cpp
@@ -287,7 +287,12 @@ void process_file_cppsym_helper(const char *filename) {
         std::set<std::string> items = ConditionalBlock::itemsOfString(expr);
         for (std::set<std::string>::const_iterator i = items.begin(); i != items.end(); i++) {
             static const boost::regex module_regexp("^(CONFIG_.*[^.])_MODULE$");
+            static const boost::regex valid_item("^[0-9A-Za-z_]+$");
             boost::match_results<std::string::const_iterator> what;
+
+            // Skip strange items like operators and rewritten CPP symbols
+            if (!boost::regex_match(*i, valid_item))
+                continue;
 
             if (boost::regex_match(*i, what, module_regexp)) {
                 found_items.insert(what[1]);

undertaker/validation/cppsym-missing-symbols.c
@@ -16,16 +16,23 @@
 #ifdef CONFIG_SND_HDA_INTEL_MODULE
 #endif
 
+#if PAGE_SIZE % 512==0
+#else
+#endif
+
 /*
  * check-name: cppsym: duplicate elimination with model and tristate symbols
  * check-command: undertaker -j cppsym -m models/x86.model $file
  * check-output-start
 I: Using x86 as primary model
 I: loaded rsf model for x86
+0 (MISSING, NOT_CONFIG_LIKE)
+512 (MISSING, NOT_CONFIG_LIKE)
 CONFIG_CGROUP_SCHED (BOOLEAN)
 CONFIG_FAIR_GROUP_SCHED (BOOLEAN)
 CONFIG_RT_GROUP_SCHED (BOOLEAN)
 CONFIG_SND_HDA_INTEL (TRISTATE)
 CONFIG_SPARC (MISSING)
+PAGE_SIZE (MISSING, NOT_CONFIG_LIKE)
  * check-output-end
  */

commit 76cb8113429969e92932b361392071a4cf00bab0

golem: clarify and fix function files_for_current_configuration
Also unbreaks 'golem -lv', which got slightly broken in commit
99e897bdde86ef295fa874713e8b5ced70d7defb

Change-Id: Ide06a7cae083e805e17f75c30d7203aafc542cd9

python/vamos/golem/kbuild.py
@@ -75,11 +75,16 @@ def files_for_current_configuration(arch=None, subarch=None, how=False):
     to be run in a Linux source tree.
 
     Returns a list of files that are compiled with the current
-    configuration. If the optional parameter 'how' is added, the
-    returned list additionally indicates in what way (i.e., module or
-    statically compiled) the file is included.
+    configuration.
 
-    If not working for the default architecture 'x86', the optional
+    The parameter 'how', which defaults to False, decides on the mode of
+    operation. If it is not set, this function guesses for each found
+    file the corresponding source file. A warning is issued if the
+    source file could not be guessed. If it is set, the function returns
+    a list of object files and additionally indicates in what way (i.e.,
+    module or statically built-in) the object file is compiled.
+
+    NB: If not working for the default architecture 'x86', the optional
     parameters "arch" (and possibly "subarch") need to be specified!
     """
 
@@ -104,15 +109,15 @@ def files_for_current_configuration(arch=None, subarch=None, how=False):
             continue
         try:
             if (how):
-                objfile = line
+                files.add(line)
             else:
                 objfile = line.split()[0]
-            # try to guess the source filename
-            sourcefile = objfile[:-2] + '.c'
-            if os.path.exists(sourcefile):
-                files.add(sourcefile)
-            else:
-                logging.warning("Failed to guess source file for %s", objfile)
+                # try to guess the source filename
+                sourcefile = objfile[:-2] + '.c'
+                if os.path.exists(sourcefile):
+                    files.add(sourcefile)
+                else:
+                    logging.warning("Failed to guess source file for %s", objfile)
         except IndexError:
             raise RuntimeError("Failed to parse line '%s'" % line)
 