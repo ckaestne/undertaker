f9682d313002ec33af0dd86966f29b0ac9492696
Christian Kaestner <you@example.com>
2014-06-07 14:06:09 +0530

    quote only constant symbols

--- scripts/kconfig/dumpconf.c
+++ scripts/kconfig/dumpconf.c
@@ -42,48 +42,66 @@ void my_expr_print(struct expr *e, void (*fn)(void *, struct symbol *, const cha
 	fn(data, NULL, "(");
 	switch (e->type) {
 	case E_SYMBOL:
-		if (e->left.sym->name)
-		fn(data, e->left.sym, e->left.sym->name);
-		else
-		fn(data, NULL, choicestring);
+		if (e->left.sym->name){
+			if ((e->left.sym->flags & SYMBOL_CONST))
+				fn(data, NULL, "'");
+			fn(data, e->left.sym, e->left.sym->name);
+			if ((e->left.sym->flags & SYMBOL_CONST))
+				fn(data, NULL, "'");
+		}else
+			fn(data, NULL, choicestring);
 		break;
 	case E_NOT:
 		fn(data, NULL, "!");
-		expr_print(e->left.expr, fn, data, E_NOT);
+		my_expr_print(e->left.expr, fn, data, E_NOT);
 		break;
 	case E_EQUAL:
-		if (e->left.sym->name)
-		fn(data, e->left.sym, e->left.sym->name);
-		else
-		fn(data, NULL, "<choice>");
-		fn(data, NULL, "='");
+		if (e->left.sym->name){
+			if ((e->left.sym->flags & SYMBOL_CONST))
+				fn(data, NULL, "'");
+			fn(data, e->left.sym, e->left.sym->name);
+			if ((e->left.sym->flags & SYMBOL_CONST))
+				fn(data, NULL, "'");
+		}else
+			fn(data, NULL, "<choice>");
+		fn(data, NULL, "=");
+		if ((e->right.sym->flags & SYMBOL_CONST))
+			fn(data, NULL, "'");
 		fn(data, e->right.sym, e->right.sym->name);
-		fn(data, NULL, "'");
+		if ((e->right.sym->flags & SYMBOL_CONST))
+			fn(data, NULL, "'");
 		break;
 	case E_UNEQUAL:
-		if (e->left.sym->name)
-		fn(data, e->left.sym, e->left.sym->name);
-		else
-		fn(data, NULL, "<choice>");
-		fn(data, NULL, "!='");
+		if (e->left.sym->name){
+			if ((e->left.sym->flags & SYMBOL_CONST))
+				fn(data, NULL, "'");
+			fn(data, e->left.sym, e->left.sym->name);
+			if ((e->left.sym->flags & SYMBOL_CONST))
+				fn(data, NULL, "'");
+		}else
+			fn(data, NULL, "<choice>");
+		fn(data, NULL, "!=");
+		if ((e->right.sym->flags & SYMBOL_CONST))
+			fn(data, NULL, "'");
 		fn(data, e->right.sym, e->right.sym->name);
-		fn(data, NULL, "'");
+		if ((e->right.sym->flags & SYMBOL_CONST))
+			fn(data, NULL, "'");
 		break;
 	case E_OR:
-		expr_print(e->left.expr, fn, data, E_OR);
+		my_expr_print(e->left.expr, fn, data, E_OR);
 		fn(data, NULL, " || ");
-		expr_print(e->right.expr, fn, data, E_OR);
+		my_expr_print(e->right.expr, fn, data, E_OR);
 		break;
 	case E_AND:
-		expr_print(e->left.expr, fn, data, E_AND);
+		my_expr_print(e->left.expr, fn, data, E_AND);
 		fn(data, NULL, " && ");
-		expr_print(e->right.expr, fn, data, E_AND);
+		my_expr_print(e->right.expr, fn, data, E_AND);
 		break;
 	case E_LIST:
 		fn(data, e->right.sym, e->right.sym->name);
 		if (e->left.expr) {
 		fn(data, NULL, " ^ ");
-		expr_print(e->left.expr, fn, data, E_LIST);
+		my_expr_print(e->left.expr, fn, data, E_LIST);
 		}
 		break;
 	case E_RANGE:
@@ -191,7 +209,7 @@ void my_print_symbol(FILE *out, struct menu *menu)
 		for_all_prompts(sym, prop) {
 			has_prompts++;
 			fprintf(out, "Prompt\t%s\t\"",itemname);
-			expr_fprint(prop->visible.expr, out);
+			my_expr_fprint(prop->visible.expr, out);
 			fprintf(out, "\"\n");
 		}
 
@@ -199,9 +217,9 @@ void my_print_symbol(FILE *out, struct menu *menu)
 
 		for_all_properties(sym, prop, P_DEFAULT) {
 			fprintf(out, "Default\t%s\t\"", itemname);
-			expr_fprint(prop->expr, out);
+			my_expr_fprint(prop->expr, out);
 			fprintf(out, "\"\t\"");
-			expr_fprint(prop->visible.expr, out);
+			my_expr_fprint(prop->visible.expr, out);
 			fprintf(out, "\"\n");
 		}
 		for_all_properties(sym, prop, P_SELECT) {
