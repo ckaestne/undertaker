836591755635d0aa40d86b399aba7751787d75c5
Stefan Hengelein <stefan.hengelein@fau.de>
2014-06-18 18:21:20 +0200

    undertaker: add temporary header file with cpp14 template methods
    With the lastest c++11 standard, several features were simply
    forgotten. i.e. make_unique<T>(param...) but make_shared<T>(param...)
    was introduced. The same happend with begin(T &t), end(T &t) and
    cbegin(T &t), cend(T &t).
    
    Thus, we temporarily add make_unique<T>(), cbegin(T &t), cend(T &t) to
    a header, which can be removed when we port to c++14.
    
    Additional Explanation:
    begin(T &t), end(T &t) return the beginning / end of a data structure
    which don't necessarily have to be iterable (like old c-arrays.
    begin/end are overloaded for old c-arrays, that's why the c++11
    for-each loop works on c-arrays).
    
    In contrast to begin/end the cbegin/cend methods return a constant
    pointer (iterator if iterable) to the beginning/end.
    
    Change-Id: I550a3d81f55a60abe2443882e3fb9507410d42ef

---
+++ undertaker/cpp14.h
@@ -0,0 +1,35 @@
+/*
+ *   undertaker - temporary header for features which will be introduced in c++14
+ *
+ * Copyright (C) 2014 Stefan Hengelein <stefan.hengelein@fau.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <memory>
+
+template<typename T, typename ...Args> std::unique_ptr<T>
+make_unique(Args && ...args) {
+    return std::unique_ptr<T>( new T(std::forward<Args>(args)... ) );
+}
+
+template<class T>
+auto cbegin(const T& t)-> decltype( t.cbegin() ) {
+    return t.cbegin();
+}
+
+template<class T>
+auto cend(const T& t)-> decltype( t.cend() ) {
+    return t.cend();
+}
